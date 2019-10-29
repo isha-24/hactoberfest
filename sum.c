0ce8117151093ab1f2c909c1e6847b2623302f5c
isha-24 <55746937+isha-24@users.noreply.github.com>
2019-10-30 00:40:42 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter number1");
+scanf("%d",&a);
+printf("Enter number2");
+scanf("%d",&b);
+sum=a+b;
+printf("sum of %d and %d is %d",a,b,sum);
+return 0;
+}
